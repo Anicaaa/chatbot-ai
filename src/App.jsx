142b489d2f5a02737634edb380eed97c2122498b
Anica Rahman <anicarahman2009@yahoo.it>
2025-12-18 00:59:32 +0000

    chatbot skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import ChatbotIcon from './components/ChatbotIcon'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container">
+      <div className="chatbot-popup">
+        <div className="chat-header">
+          <div className="header-info">
+            <ChatbotIcon />
+            <h2>Chatbot Assistant</h2>
+          </div>
+          <button className="material-symbols-rounded">keyboard_arrow_down</button>
+        </div>
+        <div className="chat-body">
+          <div className="message bot-message">
+            <ChatbotIcon />
+            <p className="message-text">
+              Hello! <br /> I'm your Chatbot Assistant. How can I help you today?
+            </p>
+          </div>
+          <div className="message user-message">
+            <p className="message-text">
+              Hi! I have a question about your services.
+            </p>
+          </div>
+          <div className="chat-footer">
+            <form action="" className="chat-form">
+              <input className="message-input" type="text" placeholder="Type your message..." required/>
+              <button className="material-symbols-rounded">arrow_upward</button>
+            </form>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
